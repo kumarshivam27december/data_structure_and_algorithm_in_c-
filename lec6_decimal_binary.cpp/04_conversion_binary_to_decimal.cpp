ecccdfad5615ce698d2b1e142968356e87bc0517
Shivam Kumar <kumarshivam16062004@gmail.com>
2023-06-11 15:59:29 +0530

    version 9.0

--- lec6_decimal_binary.cpp/04_conversion_binary_to_decimal.cpp
+++ lec6_decimal_binary.cpp/04_conversion_binary_to_decimal.cpp
@@ -1,6 +1,19 @@
 #include <iostream>
+#include <math.h>
 using namespace std;
 int main()
 {
-    
+    int num;
+    cin>>num;
+    int ans=0;
+    int i=0;
+    while(num!=0)
+    {
+      int digit = num%10;
+      ans = ans+digit*pow(2,i);
+      num/=10;
+      i++;
+    }
+    cout<<ans<<endl;
+
 }
