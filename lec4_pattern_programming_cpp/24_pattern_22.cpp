d6ff2cbfbcd89634845e5d744836c84f986b4e85
Shivam Kumar <kumarshivam16062004@gmail.com>
2023-06-11 07:54:31 +0530

    version 5.0

---
+++ lec4_pattern_programming_cpp/24_pattern_22.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    int i = 1;
+    while (i <= n)
+    {
+        int space = 1;
+        while (space <= i - 1)
+        {
+            cout << " ";
+            space++;
+        }
+
+        int j = n - i + 1;
+        while (j >= 1)
+        {
+            cout << j;
+            j--;
+        }
+
+        cout << endl;
+        i++;
+    }
+
+    return 0;
+}
