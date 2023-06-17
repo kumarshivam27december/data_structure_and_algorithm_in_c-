34a639fbb9f05069172b59466bd566bac15f06fe
Shivam Kumar <kumarshivam16062004@gmail.com>
2023-06-17 21:37:11 +0530

    version 26.0

--- lec9_introduction_to_array/01.array_declaration.cpp
+++ lec9_introduction_to_array/01.array_declaration.cpp
@@ -2,6 +2,10 @@
 using namespace std;
 int main()
 {
-    int arr[7];
+    int array[5]={0};
+    // this will inittialise every value as 0
+    // it cannot be done in the case of 1 means 1 can not be given to every inddex with this method
+    
+
     
 }
