de77b353a18cb3f9afecc735329c660e1f4fd73c
Shivam Kumar <kumarshivam16062004@gmail.com>
2023-06-18 05:21:27 +0530

    version 34.0

--- lec9_introduction_to_array/07_min_of_array.cpp
+++ lec9_introduction_to_array/07_min_of_array.cpp
@@ -1,6 +1,20 @@
 #include <iostream>
 using namespace std;
+void minArray(int arr[],int n)
+{
+    int min=INT_MAX;
+    for(int i=0;i<n;i++)
+    {
+        if(arr[i]<min)
+        {
+           min=arr[i];
+        }
+    }
+    cout<<"min of the array is "<<min<<endl;
+
+}
 int main()
 {
-    int array[10]={0,2,-9,7,-6,-3,4,-1,-22,87};
+    int array[11]={0,2,-9,7,-90,-6,-3,4,-1,-22,87};
+    minArray(array,11);
 }
