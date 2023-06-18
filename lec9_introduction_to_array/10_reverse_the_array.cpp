141fbfdabb4850237cc54c4759834ef49224a5d7
Shivam Kumar <kumarshivam16062004@gmail.com>
2023-06-18 21:45:23 +0530

    version 38.0

--- lec9_introduction_to_array/10_reverse_the_array.cpp
+++ lec9_introduction_to_array/10_reverse_the_array.cpp
@@ -1,6 +1,22 @@
 #include <iostream>
 using namespace std;
+void reverseArray(int arr[],int size)
+{
+   for(int i=size-1;i>=0;i--)
+   {
+    cout<<arr[i]<<" ";
+   }
+
+}
 int main()
 {
-    
+    int arr[5]={1,2,3,4,5};
+    cout<<"original array "<<endl;
+    for(int i=0;i<5;i++)
+    {
+        cout<<arr[i]<<" ";
+    }
+    cout<<endl;
+    reverseArray(arr,5);
+  
 }
