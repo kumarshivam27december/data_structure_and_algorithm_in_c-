103c29eebfb1a308fa12e0f8ff697a622358a785
Shivam Kumar <kumarshivam16062004@gmail.com>
2023-06-21 05:39:39 +0530

    version 49.0

---
+++ lec8_switch_case_and_function/14_fibonacci_series.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int F(int N)
+{
+    int a = 0, b = 1, c, i;
+    if( N == 0)
+        return a;
+    for(i = 2; i <= N; i++)
+    {
+       c = a + b;
+       a = b;
+       b = c;
+    }
+    return b;
+}
+// Driver code
+int main()
+{
+    int N = 5;
+     
+    cout << F(N);
+    return 0;
+}
