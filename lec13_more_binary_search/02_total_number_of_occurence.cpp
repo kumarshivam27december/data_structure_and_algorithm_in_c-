8e2256dc33612a25b897dd0ffc62b8e0ef581986
Shivam Kumar <kumarshivam16062004@gmail.com>
2023-06-23 23:34:09 +0530

    version 52.0

--- lec13_more_binary_search/02_total_number_of_occurence.cpp
+++ lec13_more_binary_search/02_total_number_of_occurence.cpp
@@ -1,18 +1,65 @@
 #include <iostream>
 using namespace std;
-int FirstOccurence(int arr[],int n, int x)
+int FirstOccurence(int arr[],int n, int key)
 {
     int s=0;
     int e = n-1;
     int mid = s + (e-s)/2;
+    int ans = -1;
     while(s<e)
     {
+        if(arr[mid]==key)
+        {
+
+            e = mid -1;
+            ans = mid;
+
+        }
+        else if (arr[mid]>key)
+        {
+            /* code */
+            e = mid -1;
+        }
+        else if (arr[mid]<key)
+        {
+            /* code */
+            s = mid +1;
+        }
+        
+        mid = s + (e-s)/2;
         
     }
+    return ans;
 }
-int LastOccurence(int arr[],int n,int x)
+int LastOccurence(int arr[],int n,int key)
 {
+    int s=0;
+    int e = n-1;
+    int mid = s + (e-s)/2;
+    int ans = -1;
+    while(s<e)
+    {
+        if(arr[mid]==key)
+        {
+           s = mid +1; 
+            ans = mid;
 
+        }
+        else if (arr[mid]>key)
+        {
+            /* code */
+            e = mid -1;
+        }
+        else if (arr[mid]<key)
+        {
+            /* code */
+            s = mid +1;
+        }
+        
+        mid = s + (e-s)/2;
+        
+    }
+    return ans;
 }
 int main()
 {
