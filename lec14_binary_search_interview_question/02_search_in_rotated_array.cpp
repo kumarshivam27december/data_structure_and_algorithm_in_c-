00db9551822833d337b4b052ab82f5448f1f6fb0
Shivam Kumar <kumarshivam16062004@gmail.com>
2023-06-27 17:42:23 +0530

    version 59.0

---
+++ lec14_binary_search_interview_question/02_search_in_rotated_array.cpp
@@ -0,0 +1,73 @@
+#include <iostream>
+using namespace std;
+int getpivot(int arr[],int n)
+{
+    int s = 0;
+    int e = n-1;
+    int  mid = s + (e-s)/2;
+    while (s<e)
+    {
+        if(arr[mid]>arr[0])
+        {
+            s = mid +1;  
+        }
+        else{
+            e = mid;
+        }
+
+
+        mid = s + (e-s)/2;
+        /* code */
+    }
+
+    return s ; // we can also return e 
+    
+}
+int binarySearch(int arr[],int start,int end,int k)
+{
+    int s = start;
+    int e = end ;
+    int mid = s + (e-s)/2;
+    while(s<=e)
+    {
+        if(arr[mid]==k)
+        {
+            return mid;
+        }
+        else if (arr[mid]>k)
+        {
+            e = mid -1;
+            /* code */
+        }
+        else if (arr[mid]<k)
+        {
+            /* code */ s = mid +1;
+        }
+        mid = s + (e-s)/2;
+        
+    }
+    return -1;
+}
+int answer(int arr[],int n,int key)
+{
+int pivot  = getpivot(arr,7);
+    if(key>=arr[pivot]&&key<=arr[n-1])
+    {
+        return  binarySearch(arr,pivot,n-1,key);
+
+    }
+    else{ 
+        return binarySearch(arr,0,pivot-1,key);
+    }
+
+}
+int main()
+{
+    int arr[7] = {5,6,8,9,2,3,4};
+    int key ;
+    cin>>key;
+   int finalise = answer(arr,7,key);
+   cout<<finalise<<endl;
+   
+
+} 
---
+++ lec14_binary_search_interview_question/03_integer_square_root_using_binary_search.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+long long int binarySearch(int n)
+{
+    int s = 0;
+    int e = n-1;
+    long long int mid = s + (e-s)/2;
+   long long int ans  = -1;
+    while (s<=e)
+    {
+      long long  int square = mid * mid ;
+        
+        if(square==n)
+        {
+            return mid;
+        }
+        if(square<n)
+        {
+            ans = mid;
+            s= mid  +1;
+        }
+        else
+        {
+            e=mid-1;
+        }
+        
+       mid = s + (e-s)/2; /* code */
+    }
+    return ans;
+}
+int sqrt(int n)
+{
+    return binarySearch(n);
+}
+
+int main()
+{
+ int  n;
+ cin>>n;
+ int ans = sqrt(n);
+ cout<<ans<<endl;   
+} 
