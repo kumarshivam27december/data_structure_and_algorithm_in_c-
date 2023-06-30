e312d174f902d4637a5d7965c743a29dd0b2aed4
Shivam Kumar <kumarshivam16062004@gmail.com>
2023-06-30 06:47:18 +0530

    version 68.0

---
+++ lec17_bubble_sort/01_buuble_sort_optimised.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+void printArray(int arr[],int n)
+{
+    for(int i=0;i<n;i++)
+    {
+        cout<<arr[i]<<" ";
+    }
+}
+void bubblesort(int arr[],int size)
+{
+    for(int i=0;i<size;i++)
+    {
+        for(int j = 0;j<size-i;j++)
+        {
+           if(arr[j]>arr[j+1])
+           {
+            swap(arr[j],arr[j+1]);
+           }
+        }
+
+    }
+}
+int main()
+{
+    int arr[7] = {6,1,4,3,9,14,2};
+    int size = sizeof(arr)/sizeof(arr[0]);
+    bubblesort(arr,7);
+    printArray(arr,7);
+    return 0;
+}
