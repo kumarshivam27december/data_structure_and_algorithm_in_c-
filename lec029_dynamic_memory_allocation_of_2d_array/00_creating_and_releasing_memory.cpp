8e23e748bf663d905bb6705412051f93d518300d
Shivam Kumar <kumarshivam16062004@gmail.com>
2023-07-12 12:14:48 +0530

    version 106.0

---
+++ lec029_dynamic_memory_allocation_of_2d_array/00_creating_and_releasing_memory.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int row;
+    cin>>row;
+    int col;
+    cin>>col;
+
+    //creating a 2d array
+
+    int **arr =  new int*[row];
+    for(int i=0;i<row;i++)
+    {
+        arr[i] = new int[col];
+    }
+
+    //taking user input
+    for(int i=0;i<row;i++)
+    {
+        for(int j=0;j<col;j++)
+        {
+            cin>>arr[i][j];
+        }
+    }
+
+    //printing output
+    for(int i=0;i<row;i++)
+    {
+        for(int j=0;j<col;j++)
+        {
+            cout<<arr[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+
+
+    //releasing the memory
+
+
+    for(int i=0;i<row;i++)
+    {
+        delete []arr[row];
+    }
+
+    delete []arr;
+
+
+
+          return 0;
+}
