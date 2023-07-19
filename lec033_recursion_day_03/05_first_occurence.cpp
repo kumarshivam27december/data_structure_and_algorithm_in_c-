225cef8bd433ee9415a301827631352c3c95c05c
Shivam Kumar <kumarshivam16062004@gmail.com>
2023-07-19 06:06:34 +0530

    version 126.0

--- lec033_recursion_day_03/05_first_occurence.cpp
+++ lec033_recursion_day_03/05_first_occurence.cpp
@@ -1,21 +1,45 @@
 #include <iostream>
 using namespace std;
-int firstOccurence(int arr[],int size,int key)
+int firstOccurence(int arr[],int start,int end,int key)
 {
     //base case
+   
     
-    if ('\n')
+    if (start>end)
     {
-        /* code */
+       return 0;
     }
-    
+    int mid = start + (end-start);
 
+    if (key==arr[mid])
+    {
+        if (mid==start ||arr[mid-1] != key)
+        {
+            return mid;
+        }
+        else{
+            return firstOccurence(arr,start,mid-1,key);
+
+        }
+    }
+
+    if (arr[mid]>key)
+    {
+        return firstOccurence(arr,start,mid-1,key);
+    }
+    if (arr[mid]<key)
+    {
+        return firstOccurence(arr,mid+1,end,key);
+    }
+
+    return mid;
+    
 }
 int main()
 {
     int arr[8] = {1,2,3,5,5,5,9,88};
-    int key =  2;
-    int firstindex = firstOccurence(arr,8,key);
+    int key =  88;
+    int firstindex = firstOccurence(arr,0,7,key);
     cout<<firstindex<<endl;
     return 0; 
 }
