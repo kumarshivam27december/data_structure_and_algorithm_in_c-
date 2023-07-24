604adf4020a5701326b1a4f8bbb9f842552a9ab9
Shivam Kumar <kumarshivam16062004@gmail.com>
2023-07-24 20:27:36 +0530

    version 140.0

---
+++ lec039_recursion_day_10_permutation_of_string/00_permutation_of_strings.cpp
@@ -0,0 +1,64 @@
+// #include <iostream>
+// using namespace std;
+// void print(int arr[],int size)
+// {
+//     for (int i = 0; i < size; i++)
+//     {
+//         /* code */
+//         cout<<arr[i]<<" ";
+//     }cout<<endl;
+    
+// }
+// void sort(int arr[],int size)
+// {
+//     int s = 0;
+//     int mid = 0;
+//     int e = size-1;
+
+//     while (mid<=e)
+//     {
+//         switch (arr[mid])
+//         {
+//         case 0:
+//             swap(arr[s],arr[mid]);
+//             s++;
+//             mid++;
+//             break;
+//         case 1:
+//             mid++;
+//             break;
+//         case 2:
+//             swap(arr[mid],arr[e]);
+//             e--;
+//             break;
+//         }
+//     }
+    
+// }
+// int main()
+// {
+//     int arr[10] = {2,0,1,0,0,2,1,0,0,2};
+//     int size = sizeof(arr)/sizeof(arr[0]);
+//     sort(arr,size);
+//     print(arr,size);
+//     return 0;
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
