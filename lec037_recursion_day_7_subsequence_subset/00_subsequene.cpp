e90f6ef100d87eab065b7844538cf89f59c0007c
Shivam Kumar <kumarshivam16062004@gmail.com>
2023-07-24 21:55:42 +0530

    version 142.0

--- lec037_recursion_day_7_subsequence_subset/00_subsequene.cpp
+++ lec037_recursion_day_7_subsequence_subset/00_subsequene.cpp
@@ -3,25 +3,38 @@
 using namespace std;
 
 void solve(vector<string>& ans, string str, string output, int i) {
-    //base case
-    if(i>=str.length()) {
-        if(output.length()>0)
-        ans.push_back(output);
-        return ;
-        }
-    
-    //exclude
-    solve(ans, str, output, i+1);
-    //include
+    // Base case
+    if (i >= str.length()) {
+        if (output.length() > 0)
+            ans.push_back(output);
+        return;
+    }
+
+    // Exclude
+    solve(ans, str, output, i + 1);
+
+    // Include
     output.push_back(str[i]);
-    solve(ans, str, output, i+1);
+    solve(ans, str, output, i + 1);
 }
 
-vector<string> subsequences(string str){
-	
-	vector<string> ans;
+vector<string> subsequences(string str) {
+    vector<string> ans;
     string output = "";
-    solve(ans,str,output,0);
+    solve(ans, str, output, 0);
     return ans;
-	
+}
+
+int main() {
+    string input_str = "abc"; // Example input
+
+    vector<string> result = subsequences(input_str);
+
+    // Output the subsequences
+    for (const string& subseq : result) {
+        cout << subseq << " ";
+    }
+    cout << endl;
+
+    return 0;
 }
--- lec037_recursion_day_7_subsequence_subset/01_subset.cpp
+++ lec037_recursion_day_7_subsequence_subset/01_subset.cpp
@@ -1,33 +1,53 @@
 #include <iostream>
 #include <vector>
 using namespace std;
+
 class Solution {
 private:
-    void solve(vector<int> nums, vector<int> output, int index, vector<vector<int> >& ans) {
-        //base case
-        if(index >= nums.size()) {
+    void solve(vector<int> nums, vector<int> output, int index, vector<vector<int>>& ans) {
+        // Base case
+        if (index >= nums.size()) {
             ans.push_back(output);
-            return ;
+            return;
         }
-        
-        //exclude
-        solve(nums, output, index+1, ans);
-        
-        //include
+
+        // Exclude
+        solve(nums, output, index + 1, ans);
+
+        // Include
         int element = nums[index];
         output.push_back(element);
-        solve(nums, output, index+1, ans);
-        
+        solve(nums, output, index + 1, ans);
     }
-    
+
 public:
     vector<vector<int>> subsets(vector<int>& nums) {
-        
-        vector<vector<int> > ans;
+        vector<vector<int>> ans;
         vector<int> output;
         int index = 0;
         solve(nums, output, index, ans);
         return ans;
-        
     }
 };
+
+int main() {
+    Solution solution;
+    vector<int> nums = {1, 2, 3}; // Example input
+
+    vector<vector<int>> result = solution.subsets(nums);
+
+    // Output the subsets
+    for (const vector<int>& subset : result) {
+        cout << "[";
+        for (size_t i = 0; i < subset.size(); ++i) {
+            cout << subset[i];
+            if (i < subset.size() - 1) {
+                cout << ", ";
+            }
+        }
+        cout << "] ";
+    }
+    cout << endl;
+
+    return 0;
+}
