9205a847f938b8e62c87470143ccf83f7fbf438b
Shivam Kumar <kumarshivam16062004@gmail.com>
2023-09-08 15:30:12 +0530

    version 200.0

--- lec050_linked_list/00_sytax.cpp
+++ lec050_linked_list/00_sytax.cpp
@@ -10,10 +10,53 @@ class Node{
         this->next = NULL;
     }
 };
+
+
+void insertAtHead(Node* &head,int d)
+{
+    //new node create
+    Node* temp = new Node(d);
+    temp->next = head;
+    head = temp;
+}
+
+void print(Node* &head)
+{
+    Node* temp = head;
+    while(temp!=NULL)
+    {
+        cout<<temp->data<<" ";
+        temp= temp->next;
+    }cout<<endl;
+}
 int main()
 {
+    //created new node
     Node* node1 = new Node(10);
     cout<<node1->data<<endl;
-    cout<<node1->next<<endl;
+    //// cout<<node1->next<<endl;
+
+
+    //head pointer to node1
+    Node* head = node1;
+    print(head);
+    
+    //insert at head
+    insertAtHead(head,11);
+    print(head);
+
+    insertAtHead(head,12);
+    print(head);
+
+    insertAtHead(head,13);
+    print(head);
+
+    insertAtHead(head,14);
+
+    print(head);
+    
+
+
+
     return 0;
 }
