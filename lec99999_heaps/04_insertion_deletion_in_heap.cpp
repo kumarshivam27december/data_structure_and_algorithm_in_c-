37546a20017b19467896cf2e01ec80e34987f404
Shivam Kumar <kumarshivam16062004@gmail.com>
2023-11-03 21:18:47 +0530

    version 224.0

--- lec99999_heaps/04_insertion_deletion_in_heap.cpp
+++ lec99999_heaps/04_insertion_deletion_in_heap.cpp
@@ -132,6 +132,43 @@ class heap{
         
     }
 };
+
+void heapify(int arr[],int size,int i){
+    int largest = i;
+    int left = 2*i;
+    int right = 2*i+1;
+
+    if (left <= size && arr[largest]< arr[left])
+    {
+        largest = left;
+    }
+
+    if (right<= size && arr[largest] < arr[right])
+    {
+        largest = right;
+    }
+
+    if (largest != i)
+    {
+        swap(arr[largest],arr[i]);
+        heapify(arr,size,largest);
+    }
+    
+    
+    
+}
+
+void heapsort(int arr[],int size){
+    int n = size;
+    while(n>1){
+        //step 1 
+        swap(arr[n],arr[1]);
+        n--;
+
+        //step 2
+        heapify(arr,n,1);
+    }
+}
 int main()
 {
     heap h;
@@ -145,5 +182,34 @@ int main()
     h.print();
     h.deletion(54);
     h.print();
+
+
+
+    //heap creation
+    int arr1[6] = {-1,50,55,53,52,54};
+    int n = 5;
+    for (int i = n/2; i > 0 ; i--)
+    {
+        heapify(arr1,n,i);
+    }
+
+
+    cout<<"printing the array now"<<endl;
+
+
+    for (int i = 1; i <= n; i++)
+    {
+       cout<<arr1[i]<<" ";
+    }cout<<endl;
+
+
+    //heapsort
+    cout<<"After heap sort"<<endl;
+    heapsort(arr1,n);
+    for (int i = 1; i <= n; i++)
+    {
+       cout<<arr1[i]<<" ";
+    }cout<<endl;
+
     return 0;
 }
