5935b85fdcd702d0a1ffad9fd3accf7c7bad15e1
Shivam Kumar <kumarshivam16062004@gmail.com>
2023-11-24 13:55:55 +0530

    version 246.0

--- complete_stl_in_one_go/project.cpp
+++ complete_stl_in_one_go/project.cpp
@@ -14,4 +14,50 @@ creating a text  based game
 --using function switch and loops
 
 
+
+
+
+///for example for kbc game
+
+#include <iostream>
+using namespace std;
+
+void askQuestion(string question, string optionA, string optionB, string optionC, string optionD, char correctAnswer, int& score) {
+    cout << question << endl;
+    cout << "A. " << optionA << endl;
+    cout << "B. " << optionB << endl;
+    cout << "C. " << optionC << endl;
+    cout << "D. " << optionD << endl;
+
+    char userAnswer;
+    cout << "Your answer: ";
+    cin >> userAnswer;
+
+    if (userAnswer == correctAnswer) {
+        cout << "Correct!" << endl;
+        score += 1000;
+    } else {
+        cout << "Incorrect! The correct answer is " << correctAnswer << endl;
+    }
+}
+
+int main() {
+    int score = 0;
+    char play;
+    do {
+        cout << "Welcome to Who Wants to Be a Millionaire!" << endl;
+        cout << "Question 1:" << endl;
+        askQuestion("What is the capital of France?", "London", "Paris", "Berlin", "Rome", 'B', score);
+
+        cout << endl << "Your total score is: " << score << " points." << endl;
+        cout << "Do you want to play again? (Y/N): ";
+        cin >> play;
+    } while (play == 'Y' || play == 'y');
+
+    cout << "Thanks for playing! Your final score is: " << score << " points." << endl;
+
+    return 0;
+}
+
+
 */
