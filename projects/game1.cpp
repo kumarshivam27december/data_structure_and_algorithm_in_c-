a4a1ac60c3a55b7401abed41cdc1f966fcc2e5f3
Shivam Kumar <kumarshivam16062004@gmail.com>
2023-11-27 01:54:50 +0530

    version 248.0

--- projects/game1.cpp
+++ projects/game1.cpp
@@ -10,22 +10,51 @@
 using namespace std;
 int main(){
 
+    cout<<"^^^^^^^^^^^^^^^^^^^^^^^^^^^"<<endl;
+    cout<<"^^^^^^^^^^^^^^^^^^^^^^^^^^^"<<endl;
+
     cout<<"---------------------------"<<endl;
     cout<<"WELCOME TO A WONDERFUL GAME"<<endl;
     cout<<"---------------------------"<<endl;
     cout<<"WHO WANTS TO BE MILLIONARE "<<endl;
     cout<<"---------------------------"<<endl;
+    cout<<"---------------------------"<<endl;
+
     cout<<" You need to know the rule "<<endl;
     cout<<"First Rule :"<<endl;
     cout<<"You must follow all the rule strictly"<<endl;
     cout<<"Second rule:"<<endl;
     cout<<"You have to give answer in limited time"<<"\n";
     cout<<"You will be given 7 questions and you have to answer it"<<"\n";
+    cout<<"1qn->$1000\n2qn->$5000\n3qn->$20,000\n4qn->$50,000\n5qn->$100,000\n6qn->$500,000\n7qn->1,000,000\n";
+    cout<<endl;
+    cout<<"You have option to leave the game in the middle on question 3rd,5th and 6th"<<endl;
+    cout<<"---------------------------"<<endl;
+    cout<<"---------------------------"<<endl;
+    
     cout<<"You will be given assistance or lifeline"<<"\n";
-    cout<<"First "
+    cout<<"1-> 50:50 (Two of the options will be eliminated)"<<endl; 
+    cout<<"2->phone your friend(you can ask your friend answer)"<<endl;
+    cout<<"3->replace the question(Another question will be provided)"<<endl;
+    cout<<"4->Exit the game with 3/4 of amount(75 % of the amount)"<<endl;
 
 
+    cout<<"---------------------------"<<endl;
+    cout<<"---------------------------"<<endl;
 
+    cout<<"LETS BEGIN THE GAME"<<endl;
+    cout<<"^^^^^^^^^^^^^^^^^^^^^^^^^^^"<<endl;
+    cout<<"^^^^^^^^^^^^^^^^^^^^^^^^^^^"<<endl;
+
+    cout<<"Do you want start the game \nif yes press Y else press N\n";
+    char start;cin>>start;
+    if(start=='Y' || start=='y'){
+        cout<<"Starting the game\n";
+        //code here
+    }else{
+        cout<<"Thanks for you response\nExiting the game\n";
+        return 1;
+    }
 
     return 0;
 }
