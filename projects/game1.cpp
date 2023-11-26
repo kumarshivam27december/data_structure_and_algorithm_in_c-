0bf7ba4f81e9bf9ec67f83e50cbb51406c934c53
Shivam Kumar <kumarshivam16062004@gmail.com>
2023-11-27 01:28:42 +0530

    version 247.0

--- projects/game1.cpp
+++ projects/game1.cpp
@@ -9,6 +9,23 @@
 #include <bits/stdc++.h>
 using namespace std;
 int main(){
-    
+
+    cout<<"---------------------------"<<endl;
+    cout<<"WELCOME TO A WONDERFUL GAME"<<endl;
+    cout<<"---------------------------"<<endl;
+    cout<<"WHO WANTS TO BE MILLIONARE "<<endl;
+    cout<<"---------------------------"<<endl;
+    cout<<" You need to know the rule "<<endl;
+    cout<<"First Rule :"<<endl;
+    cout<<"You must follow all the rule strictly"<<endl;
+    cout<<"Second rule:"<<endl;
+    cout<<"You have to give answer in limited time"<<"\n";
+    cout<<"You will be given 7 questions and you have to answer it"<<"\n";
+    cout<<"You will be given assistance or lifeline"<<"\n";
+    cout<<"First "
+
+
+
+
     return 0;
 }
