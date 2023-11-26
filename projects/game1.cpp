cc36da4ce9fcc0726445deb1f024b1f9c473faae
Shivam Kumar <kumarshivam16062004@gmail.com>
2023-11-27 02:02:41 +0530

    version 249.0

--- projects/game1.cpp
+++ projects/game1.cpp
@@ -7,12 +7,25 @@
 #include <stack>
 #include <math.h>
 #include <bits/stdc++.h>
+#include <windows.h>
+#include <stdlib.h>
 using namespace std;
 int main(){
 
+
+    HANDLE console_color; 
+    
+    console_color = GetStdHandle( STD_OUTPUT_HANDLE); 
+
+
+SetConsoleTextAttribute(console_color, 1); 
+
+
     cout<<"^^^^^^^^^^^^^^^^^^^^^^^^^^^"<<endl;
     cout<<"^^^^^^^^^^^^^^^^^^^^^^^^^^^"<<endl;
 
+
+ SetConsoleTextAttribute(console_color, 2); 
     cout<<"---------------------------"<<endl;
     cout<<"WELCOME TO A WONDERFUL GAME"<<endl;
     cout<<"---------------------------"<<endl;
@@ -20,6 +33,7 @@ int main(){
     cout<<"---------------------------"<<endl;
     cout<<"---------------------------"<<endl;
 
+ SetConsoleTextAttribute(console_color, 3); 
     cout<<" You need to know the rule "<<endl;
     cout<<"First Rule :"<<endl;
     cout<<"You must follow all the rule strictly"<<endl;
@@ -32,6 +46,10 @@ int main(){
     cout<<"---------------------------"<<endl;
     cout<<"---------------------------"<<endl;
     
+
+ SetConsoleTextAttribute(console_color, 4); 
+
+
     cout<<"You will be given assistance or lifeline"<<"\n";
     cout<<"1-> 50:50 (Two of the options will be eliminated)"<<endl; 
     cout<<"2->phone your friend(you can ask your friend answer)"<<endl;
@@ -39,19 +57,22 @@ int main(){
     cout<<"4->Exit the game with 3/4 of amount(75 % of the amount)"<<endl;
 
 
+ SetConsoleTextAttribute(console_color, 5); 
     cout<<"---------------------------"<<endl;
     cout<<"---------------------------"<<endl;
 
     cout<<"LETS BEGIN THE GAME"<<endl;
     cout<<"^^^^^^^^^^^^^^^^^^^^^^^^^^^"<<endl;
     cout<<"^^^^^^^^^^^^^^^^^^^^^^^^^^^"<<endl;
-
-    cout<<"Do you want start the game \nif yes press Y else press N\n";
+ SetConsoleTextAttribute(console_color, 6); 
+    cout<<"Do you want start the game \nif yes press Y or any other key to exit\n";
     char start;cin>>start;
     if(start=='Y' || start=='y'){
+         SetConsoleTextAttribute(console_color, 7); 
         cout<<"Starting the game\n";
         //code here
     }else{
+         SetConsoleTextAttribute(console_color, 8); 
         cout<<"Thanks for you response\nExiting the game\n";
         return 1;
     }
