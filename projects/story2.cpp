68c6f87b6bc6c2391e370b6ba0d25a1c0bc7d23f
Shivam Kumar <kumarshivam16062004@gmail.com>
2023-12-11 14:43:42 +0530

    version 270.0

--- projects/story2.cpp
+++ projects/story2.cpp
@@ -1,30 +1,51 @@
 /*
 
-"It is late night, not too late but it's quite late to sleep, but I wish to watch T.V all night because it's a chance to use my time as I am home alone", I spoke to myself as I was watching the T.V all the way alone.
+"It is late night, not too late but it's quite late to sleep, 
+but I wish to watch T.V all night because it's a chance to use my time as
+ I am home alone", I spoke to myself as I was watching the T.V all the way alone.
 
 
 
-Yes, today's night is a home alone night. My mom and dad left for some important function and I was not invited, even my parents didn't wish to take me. So, they gave me a mobile and a tv with my table full of snacks. You know it is always an awesome feeling being alone. We can do what we want, we can be free as a bird. No one can stop us from doing anything.
+Yes, today's night is a home alone night. 
+My mom and dad left for some important function and I was not invited, 
+even my parents didn't wish to take me. So, they gave me a mobile and a 
+tv with my table full of snacks. You know it is always an awesome feeling being alone. 
+We can do what we want, we can be free as a bird. No one can stop us from doing anything.
 
 
 
-It was late 10:00 pm. This experience of watching a horror movie named 'Insidious at late-night is super awesome.
+It was late 10:00 pm. This experience of watching a horror movie named 
+'Insidious at late-night is super awesome.
 
 
 
-I was seeing the movie with an ocean of interest. So,I was totally leaned in that movie that I didn't even notice I got a call from my mom. I realized that I got it when I saw the phone in the 3:45 minute break. Then I chatted with my friends. Suddenly, I heard something falling in my kitchen. I turned back and found nothing. I consoled my gear saying it may be just a rat. 
+I was seeing the movie with an ocean of interest. 
+So,I was totally leaned in that movie that I didn't even notice I got a call from my mom. 
+I realized that I got it when I saw the phone in the 3:45 minute break. 
+Then I chatted with my friends. Suddenly, I heard something falling in my kitchen. 
+I turned back and found nothing. I consoled my gear saying it may be just a rat. 
 
 
 
-I just gave a look at the window that is to the side of the door and in the direction to my left hand. To my horror, I saw a man catching a knife and looking at me with his red eyes.
+I just gave a look at the window that is to the side of the door and 
+in the direction to my left hand. To my horror, I saw a man catching a 
+knife and looking at me with his red eyes.
 
 
 
-Chills ran down my spine and I froze. I didn't look around and immediately hid under my sofa. I didn't know what to do. I peeped through the small hole of the sofa and my blood froze as the man was still standing but with a creepy smile. I guess I haven't seen this type of smile in any horror movie. It's scarier than that.
+Chills ran down my spine and I froze. I didn't look around and 
+immediately hid under my sofa. I didn't know what to do.
+ I peeped through the small hole of the sofa and my blood froze
+  as the man was still standing but with a creepy smile. 
+  I guess I haven't seen this type of smile in any horror movie. 
+  It's scarier than that.
 
 
 
-I gulped and looked at my phone that I was holding in my hand. I took a long breath and called the police. The call kept ringing, I kept praying to the God to lift the call. My heart pumped a hundred times faster as they still didn't lift my call and it was ringing.
+I gulped and looked at my phone that I was holding in my hand. 
+I took a long breath and called the police. The call kept ringing,
+ I kept praying to the God to lift the call. My heart pumped a hundred times 
+ faster as they still didn't lift my call and it was ringing.
 
 
 
@@ -56,11 +77,14 @@ I gulped and looked at my phone that I was holding in my hand. I took a long bre
 
 
 
-I was waiting on the call and I peeped the second time and my heart came to a normal beat when I didn't see the man.
+I was waiting on the call and I peeped the second time 
+and my heart came to a normal beat when I didn't see the man.
 
 
 
-"Mam,the police are arriving in 10 minutes. Please be in contact with me until they arrive", the lady spoke giving me some courage to breathe.
+"Mam,the police are arriving in 10 minutes. 
+Please be in contact with me until they arrive", 
+the lady spoke giving me some courage to breathe.
 
 
 
@@ -76,27 +100,37 @@ I breathe as hard as I can.
 
 
 
-I heard police sirens and there were knocks on my door with a lady's voice asking, "Anybody there?"
+I heard police sirens and there were knocks on my door with a lady's voice asking, 
+"Anybody there?"
 
 
 
-Happy tears dwelled in my eyes and I ran to the door unlocking it. I immediately hugged the policewoman who was in front of me. 
+Happy tears dwelled in my eyes and I ran to the door unlocking it. 
+I immediately hugged the policewoman who was in front of me. 
 
 
 
-She consoled me by rubbing my back saying, "That's okay we are here. Nothing to worry my girl." 
+She consoled me by rubbing my back saying, "That's okay we are here. 
+Nothing to worry my girl." 
 
 
 
-After a few seconds, I unwrapped my arms around her and moved so that they could investigate the house. The lady police officer was with me, standing and the other policers were wandering around the house.
+After a few seconds, I unwrapped my arms around her and moved so that 
+they could investigate the house. The lady police officer was with me, 
+standing and the other policers were wandering around the house.
 
 
 
-I didn't observe the whole house except just watching the tv which was still showing insidious.
+I didn't observe the whole house except just watching the tv which was 
+still showing insidious.
 
 
 
-After a few minutes, the police made me show all my house and I was totally shocked that I might have died just because of a heart attack. The house had muddy footprints starting from the storeroom, continuing in the kitchen and ended just behind my sofa where I have been sitting. I didn't understand anything.
+After a few minutes, the police made me show all my house and I 
+was totally shocked that I might have died just because of a heart attack. 
+The house had muddy footprints starting from the storeroom, 
+continuing in the kitchen and ended just behind my sofa where I have been sitting. 
+I didn't understand anything.
 
 
 
@@ -124,11 +158,15 @@ Kail: "So how did you see the intruder?"
 
 
 
-Me: "I was watching TV but suddenly I got some sounds from the kitchen. But when I turned back I found nothing. After a few seconds, I gave a look at my nearest side window and was shocked to see that there was a man outside the window."
+Me: "I was watching TV but suddenly I got some sounds from the kitchen. 
+But when I turned back I found nothing. After a few seconds, 
+I gave a look at my nearest side window and was shocked to see 
+that there was a man outside the window."
 
 
 
-Kail: "Okay...so what you have said is exactly matching to our expectations but there is a slight change that may shock you."
+Kail: "Okay...so what you have said is exactly matching to our 
+expectations but there is a slight change that may shock you."
 
 
 
@@ -136,11 +174,13 @@ Me: "What?? What change?"
 
 
 
-Kail: "The man or the intruder whom you saw in the transparent window is not outside the window but inside the window; inside the house."
+Kail: "The man or the intruder whom you saw in the transparent 
+window is not outside the window but inside the window; inside the house."
 
 
 
-My mouth was hanging with my eyes widened and my breathing stopped for a second.
+My mouth was hanging with my eyes widened and my 
+breathing stopped for a second.
 
 
 
@@ -152,19 +192,26 @@ Kail: "Yes Shruti. Let me show you an example."
 
 
 
-She said as she called a man police officer and made him stand on the last footprint that was behind the sofa. I was made to sit in the exact before position. I looked at the window and was thrilled that his reflection was like he was outside the window but not inside.
+She said as she called a man police officer and 
+made him stand on the last footprint that was behind the sofa. 
+I was made to sit in the exact before position. 
+I looked at the window and was thrilled that his 
+reflection was like he was outside the window but not inside.
 
 
 
-I turned towards officer Kail. She smiled at me and asked, "Looking like it was outside?"
+I turned towards officer Kail. 
+She smiled at me and asked, "Looking like it was outside?"
 
 
 
-I nod my head and asked, "I realize he was inside; behind me; but how did he break into the house?"
+I nod my head and asked, "I realize he was inside; 
+behind me; but how did he break into the house?"
 
 
 
-"He broke your storeroom which was really weak", Kail said as she showed me the rusted lock. 
+"He broke your storeroom which was really weak", 
+Kail said as she showed me the rusted lock. 
 
 
 
@@ -172,10 +219,16 @@ I nod my head and asked, "I realize he was inside; behind me; but how did he bre
 
 
 
-"That is what we are thinking. But by God's grace, you were saved. We will inform your mother and father about this. You can take rest sitting here, we will be here until your mother and father arrive", she said as she was searching for something in her pocket. Maybe a phone to call my parents.
+"That is what we are thinking. But by God's grace, 
+you were saved. We will inform your mother and father about this. 
+You can take rest sitting here, we will be here until your mother 
+and father arrive", she said as she was searching for something in her pocket. 
+Maybe a phone to call my parents.
 
 
 
-I sat down on the sofa and was just blankly looking at the window wondering why that intruder didn't kill me. I don't why he didn't, even when he had a chance but I believe it was by God's grace...
+I sat down on the sofa and was just blankly looking at the window wondering 
+why that intruder didn't kill me. I don't why he didn't, even when he had a 
+chance but I believe it was by God's grace...
 
 */
