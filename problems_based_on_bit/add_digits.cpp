88d0bd479fee738ce7865ac8eeb42b035f2875b7
Shivam Kumar <kumarshivam16062004@gmail.com>
2024-02-07 20:38:47 +0530

    adding hard problems

--- problems_based_on_bit/add_digits.cpp
+++ problems_based_on_bit/add_digits.cpp
@@ -11,7 +11,20 @@ using namespace std;
 class Solution {
 public:
     vector<int> plusOne(vector<int>& digits) {
-        
+        int n = digits.size();
+        digits[n-1]+=1;
+        int carry = 0;
+        for(int i = n-1;i>=0;i--){
+            digits[i]+=carry;
+            carry = digits[i]/10;
+            digits[i] = digits[i]%10;
+        }
+
+        if(carry){
+            digits.insert(digits.begin(),carry);
+        }
+
+        return digits;
     }
 };
 int main() {
