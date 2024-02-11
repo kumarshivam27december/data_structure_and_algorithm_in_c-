35bad26a8c9c3e7264de81176b3e9284c56d13e6
Shivam Kumar <kumarshivam16062004@gmail.com>
2024-02-11 19:53:41 +0530

    candy problem leetcode

---
+++ z_hards/candy.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+int totalCandy(vector<int> &v){
+    if(v.size()==0) return 0;
+    if(v.size()==1) return 1;
+    int l = v.size();
+    vector<int> k(l,1);
+    int total = l;
+
+
+    
+    return total;
+}
+int main(){
+    vector <int> v = {1,0,2};
+    cout<<totalCandy(v)<<endl;
+    return 0;
+}
