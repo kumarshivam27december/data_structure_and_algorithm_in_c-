2ffe31884978ca5f1ae94fa71d73d674b49a75f5
Shivam Kumar <kumarshivam16062004@gmail.com>
2024-02-15 01:23:25 +0530

    is anagram leetcode solution using map

--- z_hards/is_anagram.cpp
+++ z_hards/is_anagram.cpp
@@ -3,10 +3,31 @@ using namespace std;
 bool isanagram(string s,string t){
     if(s.size()!=t.size()) return false;
 
-    int arr[26] = {0};
+   
+   /*
+    int map[26] = {0};
     for(int i = 0;i<s.size();i++){
-        
+        map[s[i]-'a']++;
+        map[t[i]-'a']--;
     }
+
+    for(auto i:map){
+        if(i !=0) return false;
+    }
+    return true;
+
+    */
+
+   map<char,int> mp;
+   for(int i =0;i<s.size();i++){
+    mp[s[i]]++;
+    mp[t[i]]--;
+   }
+
+   for(auto x :mp ){
+    if(x.second!=0) return  false;
+   }
+   return true;
 }
 int main(){
     string s = "anagram";
@@ -18,3 +39,30 @@ int main(){
     }
     return 0;
 }
+
+
+/*
+
+#include <map>
+#include <string>
+using namespace std;
+
+bool isAnagram(string s, string t) {
+    if (s.size() != t.size()) return false;
+
+    map<char, int> charCount;
+
+    for (int i = 0; i < s.size(); i++) {
+        charCount[s[i]]++;
+        charCount[t[i]]--;
+    }
+
+    for (const auto& pair : charCount) {
+        if (pair.second != 0) return false;
+    }
+
+    return true;
+}
+
+
+*/
