42170d606c2f6bd94139dbfc3d6e59d015f9850d
Shivam Kumar <kumarshivam16062004@gmail.com>
2024-02-18 03:14:18 +0530

    trapping rainwater leetcode

--- z_hards/candy.cpp
+++ z_hards/candy.cpp
@@ -1,18 +1,34 @@
 #include <bits/stdc++.h>
 using namespace std;
-int totalCandy(vector<int> &v){
-    if(v.size()==0) return 0;
-    if(v.size()==1) return 1;
-    int l = v.size();
-    vector<int> k(l,1);
-    int total = l;
+int totalCandy(vector<int> &rating){
+    if(rating.size()==0) return 0;
+    if(rating.size()==1) return 1;
+    int l = rating.size();
+    vector<int> candies(l,1);
 
-    //code here
+    //left to right  comparing i and i+1
+    for (int i = 1; i < l; i++)
+    {
+        if(rating[i]>rating[i-1] && candies[i]<=candies[i-1]){
+            candies[i] = candies[i-1] + 1;
+        }
+    }
     
-    return total;
+    //right to left 
+    for(int i = l-2;i>=0;i--){
+        if(rating[i+1]<rating[i] && candies[i+1]>=candies[i]){
+            candies[i]=candies[i+1]+1;
+        }
+    }
+    int totalCandy = 0;
+    for(int i = 0;i<l;i++){
+        totalCandy+=candies[i];
+    }
+    
+    return totalCandy;
 }
 int main(){
-    vector <int> v = {1,0,2};
-    cout<<totalCandy(v)<<endl;
+    vector <int> rating = {1,0,2};
+    cout<<totalCandy(rating)<<endl;
     return 0;
 }
