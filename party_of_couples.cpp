4825d2bb3f6eccee179249f10388a6751ccbe709
Shivam Kumar <kumarshivam16062004@gmail.com>
2024-04-11 02:16:23 +0530

    bit manipulation party of couples

--- party_of_couples.cpp
+++ party_of_couples.cpp
@@ -1,6 +1,12 @@
 #include <bits/stdc++.h>
 using namespace std;
 int singles(int n,vector<int>&arr){
+    int ans = 0;
+    for (int i = 0; i < n; i++)
+    {
+        ans = ans^arr[i];
+    }
+    return ans;
     
 }
 int main()
